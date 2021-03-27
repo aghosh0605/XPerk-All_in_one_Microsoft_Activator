a34d47ea128eee58488d48aa2a94daf8e879711e
aghosh0605 <aghosh0605@gmail.com>
2021-03-27 19:52:30 +0530

    Test dev Branch😪

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace Silicon_Hertz_Tool
             this.win8 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.check_os = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -130,16 +130,16 @@ namespace Silicon_Hertz_Tool
             this.label1.TabIndex = 7;
             this.label1.Text = "The One and Only Activator Tool You Need";
             // 
-            // button1
+            // check_os
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(187)))), ((int)(((byte)(240)))));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(447, 575);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(402, 75);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Check OS";
-            this.button1.UseVisualStyleBackColor = false;
+            this.check_os.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(187)))), ((int)(((byte)(240)))));
+            this.check_os.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.check_os.Location = new System.Drawing.Point(447, 575);
+            this.check_os.Name = "check_os";
+            this.check_os.Size = new System.Drawing.Size(402, 75);
+            this.check_os.TabIndex = 8;
+            this.check_os.Text = "Check OS";
+            this.check_os.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
@@ -162,7 +162,7 @@ namespace Silicon_Hertz_Tool
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.check_os);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.win8);
@@ -191,7 +191,7 @@ namespace Silicon_Hertz_Tool
         private System.Windows.Forms.Button win8;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button check_os;
         private System.Windows.Forms.Button button2;
     }
 }
